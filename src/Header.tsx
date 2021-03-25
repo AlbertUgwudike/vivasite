6e56ba15c11a5e7eae7f5072e7f386abbe214218
albert <albertu98@googlemail.com>
2021-03-25 12:14:20 +0000

    Added styling and bootstrap dependency

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,24 +4,24 @@ const Header = () =>
 {
     return (
         <nav className = "navbar navbar-expand-md">
-        <a className="navbar-brand" href="#">VIVA</a>
-        <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="main-navigation">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Projects</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">About</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+            <a className="navbar-brand" href="#">VIVA</a>
+            <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="main-navigation">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 }
 
