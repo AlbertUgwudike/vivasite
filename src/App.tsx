991f9587931afabdb2b9cf44b72eaf98d4f9ff33
albert <albertu98@googlemail.com>
2021-04-13 23:05:13 +0100

    removed redundant dependancy (React)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Header from './Header';
 import Slider from './Slider';
 import { sliderDetailType, settingsType, USSType } from './Types'
